30f3caaac54ff73e4bb7cacadd5ed559ebfe968d
Anurag19-Pandey <89677430+Anurag19-Pandey@users.noreply.github.com>
2022-12-21 17:04:44 +0530

    9th commit by Anurag Pandey "minor changes"

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../../components/user/jsx/Header'
 import AboutBanner2 from '../../components/About/jsx/AboutBanner2'
-import "../../components/About/css/AboutStyles.css";
 import Carreer from '../../components/About/jsx/Carreer';
 import Join from '../../components/About/jsx/Join';
 import Community from '../../components/About/jsx/Community';
@@ -11,22 +10,12 @@ function About() {
   return (
     <>
     <Header/>
-    <div className='about_page'>
-        <div className='banner_sections'> 
-        <AboutBanner2/>
-        </div>
-        <div className="about_second_banner_section">
-        <img src="/static/media/logo.abe88664698e890898be.png" alt="learning" />
-      </div>
-    </div>
+    <AboutBanner2/>
     <Carreer/>
     <Join/>
-   <Community/>
+    <Community/>
     <Doubt/>
     <AboutFooter/>
-    {/* 
-            
-    </div> */}
     </>
   )
 }
