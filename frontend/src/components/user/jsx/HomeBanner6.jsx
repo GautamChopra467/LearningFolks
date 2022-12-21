57fff8ee87ae950b9d80b0756b39ecb674abad86
GautamChopra467 <harshchopra467@gmail.com>
2022-12-21 09:09:03 -0800

    10th Commit by Gautam Chopra "testimonials"

--- frontend/src/components/user/jsx/HomeBanner6.jsx
+++ frontend/src/components/user/jsx/HomeBanner6.jsx
@@ -16,7 +16,7 @@ const HomeBanner6 = () => {
                     <h3>“Joining Learning Folks gave me insights of this tech world and it’s opportunities.”</h3>
                 </div>
 
-                <div className='card_bottom_section_homebanner6'>
+                {/* <div className='card_bottom_section_homebanner6'>
                     <div className='image_container_homebanner6'>
                         <img src={Testimonial1} alt='testimonial' />
                     </div>
@@ -24,7 +24,7 @@ const HomeBanner6 = () => {
                         <h4>Dr. Shagun Batra</h4>
                         <p>Rank 1</p>
                     </div>
-                </div>
+                </div> */}
             </div>
 
             <div className='card_container_homebanner6'>
@@ -32,7 +32,7 @@ const HomeBanner6 = () => {
                     <h3>“Associating with Learning Folks, it’s a great experience to have a great network of learners and experienced people”</h3>
                 </div>
 
-                <div className='card_bottom_section_homebanner6'>
+                {/* <div className='card_bottom_section_homebanner6'>
                     <div className='image_container_homebanner6'>
                         <img src={Testimonial1} alt='testimonial' />
                     </div>
@@ -40,7 +40,7 @@ const HomeBanner6 = () => {
                         <h4>Dr. Shagun Batra</h4>
                         <p>Rank 1</p>
                     </div>
-                </div>
+                </div> */}
             </div>
 
             <div className='card_container_homebanner6'>
@@ -48,7 +48,7 @@ const HomeBanner6 = () => {
                     <h3>“My college life has taken such an impactful jump, has helped me impro my soft skills and personality”</h3>
                 </div>
 
-                <div className='card_bottom_section_homebanner6'>
+                {/* <div className='card_bottom_section_homebanner6'>
                     <div className='image_container_homebanner6'>
                         <img src={Testimonial1} alt='testimonial' />
                     </div>
@@ -56,7 +56,7 @@ const HomeBanner6 = () => {
                         <h4>Dr. Shagun Batra</h4>
                         <p>Rank 1</p>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
       </div>
